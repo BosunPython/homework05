6002ea95045c46a30bd3d99bc441b0841125a424
BosunPython <Newyork303h2@gmail.com>
2022-12-08 11:05:16 +0300

    "add new homework"

--- Lessons05/01.py
+++ Lessons05/01.py
@@ -1,3 +1,4 @@
+
 camera = {"Bullet": "2Mp", "Turret": "4Mp", "Dome": "5Mp"}
 brands_camera = {1: "Hikvision", 2: "Dahua", 3: "UNV"}
 def func_camera(a,b):
@@ -5,3 +6,4 @@ def func_camera(a,b):
     return result
 all_cameras = func_camera(camera, brands_camera)
 print(all_cameras)
+
