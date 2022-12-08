775e85ccb50fa40c1bb82c2a511afe6ed45324f3
BosunPython <Newyork303h2@gmail.com>
2022-12-08 10:47:15 +0300

    New homework

--- Lessons05/01.py
+++ Lessons05/01.py
@@ -1,1 +1,7 @@
-print("First Fail")
+camera = {"Bullet": "2Mp", "Turret": "4Mp", "Dome": "5Mp"}
+brands_camera = {1: "Hikvision", 2: "Dahua", 3: "UNV"}
+def func_camera(a,b):
+    result = camera | brands_camera
+    return result
+all_cameras = func_camera(camera, brands_camera)
+print(all_cameras)
